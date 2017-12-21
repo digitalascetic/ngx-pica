8c9cdc4076454b9a5740d6afc9789634b1e1d258
Eduard Deza Castellano <ed@ascetic.io>
2017-12-21 16:36:41 +0100

    Return Observable<File> for each resized image

--- src/ngx-pica.service.ts
+++ src/ngx-pica.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import pica from 'pica/dist/pica';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
 
 declare let window: any;
 
@@ -7,16 +9,23 @@ declare let window: any;
 export class NgxPicaService {
 
 
-    public resizeImages(files: File[], width: number, height: number, keepAspectRatio: boolean = false) {
-
+    public resizeImages(files: File[], width: number, height: number, keepAspectRatio: boolean = false): Observable<File> {
+        const resizedImage: Subject<File> = new Subject();
         const totalFiles: number = files.length;
 
         for (let i = 0; i < totalFiles; i++) {
-            this.resizeImage(files[i], width, height, keepAspectRatio);
+            this.resizeImage(files[i], width, height, keepAspectRatio).subscribe(imageResized => {
+                resizedImage.next(imageResized);
+            }, (err) => {
+                resizedImage.error(err);
+            });
         }
+
+        return resizedImage.asObservable();
     }
 
-    public resizeImage(file: File, width: number, height: number, keepAspectRatio: boolean = false) {
+    public resizeImage(file: File, width: number, height: number, keepAspectRatio: boolean = false): Observable<File> {
+        const resizedImage: Subject<File> = new Subject();
         const originCanvas: HTMLCanvasElement = document.createElement('canvas');
         const ctx = originCanvas.getContext('2d');
         const img = new Image();
@@ -46,16 +55,20 @@ export class NgxPicaService {
                 destinationCanvas.height = height;
 
                 resizer.resize(originCanvas, destinationCanvas)
+                    .catch((err) => resizedImage.error(err))
                     .then((resizedCanvas: HTMLCanvasElement) => resizer.toBlob(resizedCanvas, file.type))
                     .then((blob: Blob) => {
                         let fileResized: File = this.generateResultFile(blob, file.name, file.type, new Date().getTime());
+                        resizedImage.next(fileResized);
                     });
             };
 
             img.src = window.URL.createObjectURL(file);
-        }else{
-            throw 'Canvas context identifier is not supported.';
+        } else {
+            resizedImage.error('Canvas context identifier is not supported.');
         }
+
+        return resizedImage.asObservable();
     }
 
     private generateResultFile(blob: Blob, name: string, type: string, lastModified: number): File {
