fad44c31b809428680955fc7653079dffd2c0cdd
Eduard Deza Castellano <ed@ascetic.io>
2018-07-31 16:45:38 +0200

    Avoid read Exif data

--- src/ngx-pica.service.ts
+++ src/ngx-pica.service.ts
@@ -169,21 +169,18 @@ export class NgxPicaService {
 
             if (ctx) {
                 img.onload = () => {
-                    this._ngxPicaExifService.getExifOrientedImage(img).then(orientedImage => {
-                        window.URL.revokeObjectURL(img.src);
-                        originCanvas.width = orientedImage.width;
-                        originCanvas.height = orientedImage.height;
+                    originCanvas.width = img.width;
+                    originCanvas.height = img.height;
 
-                        ctx.drawImage(orientedImage, 0, 0);
+                    ctx.drawImage(img, 0, 0);
 
-                        this.getCompressedImage(originCanvas, file.type, 1, sizeInMB, 0)
-                            .catch((err) => compressedImage.error(err))
-                            .then((blob: Blob) => {
-                                const imgCompressed: File = this.blobToFile(blob, file.name, file.type, new Date().getTime());
+                    this.getCompressedImage(originCanvas, file.type, 1, sizeInMB, 0)
+                        .catch((err) => compressedImage.error(err))
+                        .then((blob: Blob) => {
+                            const imgCompressed: File = this.blobToFile(blob, file.name, file.type, new Date().getTime());
 
-                                compressedImage.next(imgCompressed);
-                            });
-                    });
+                            compressedImage.next(imgCompressed);
+                        });
                 };
 
                 img.src = window.URL.createObjectURL(file);
@@ -218,7 +215,7 @@ export class NgxPicaService {
         step: number
     ): Promise<Blob> {
         return new Promise<Blob>((resolve,
-            reject) => {
+                                  reject) => {
 
             if (step > this.MAX_STEPS) {
                 reject(NgxPicaErrorType.NOT_BE_ABLE_TO_COMPRESS_ENOUGH);
